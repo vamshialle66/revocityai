5808b195921932cd01d5d7e9ba8cc11fb0405665
Vamshi Alle <vamshialle66@gmail.com>
2026-01-02 13:41:01 +0530

    Wrap AppWithAuth in NotificationProvider

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "@/contexts/AuthContext";
+import { NotificationProvider } from "@/contexts/NotificationContext";
 import Navbar from "@/components/Navbar";
 import Index from "./pages/Index";
 import Dashboard from "./pages/Dashboard";
@@ -152,7 +153,9 @@ const App = () => (
       <Toaster />
       <Sonner />
       <AuthProvider>
-        <AppWithAuth />
+        <NotificationProvider>
+          <AppWithAuth />
+        </NotificationProvider>
       </AuthProvider>
     </TooltipProvider>
   </QueryClientProvider>
